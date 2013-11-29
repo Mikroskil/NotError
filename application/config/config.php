faf3c39425f9c9978ff1302ed4d7b9c2cdd07f18
RiduanMikroskil <111111900@students.mikroskil.ac.id>
2013-11-29 18:29:51 +0700

    setting baru cms

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/mynoterror/';
+$config['base_url']	= 'http://localhost/NotError/';
 
 /*
 |--------------------------------------------------------------------------
