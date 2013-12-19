7bba790aa436b09dc8dd5e0d65628ababe547f05
RiduanMikroskil <111111900@students.mikroskil.ac.id>
2013-12-19 14:33:30 +0700

    Setting untuk theme

---
+++ application/controllers/setting.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * 
+ */
+class Setting extends CI_Controller {  
+    function __construct() {
+        parent ::__construct();
+    }
+    
+    function sethome($id){
+        cekLogin();
+        
+        setSetting('HomePageID', $id);
+        redirect('page');
+        
+    }
+    
+    function general(){
+        cekLogin();
+        $data['title'] = 'General Setting';
+        
+        $this -> load -> view('setting/general', $data);  
+   
+    }
+    
+    function email(){
+        cekLogin();
+        $data['title']  = 'Email Setting';
+        
+        $this -> load -> view('setting/email', $data);
+    }
+    
+    
+    function logo(){
+        cekLogin();
+        $data['title'] = 'Logo';
+        $rules = array(
+                        array(
+                            'field'=>'MediaPath',
+                            'label'=>'Logo',
+                            'rules'=>'required'
+                        )
+        );
+        $this->form_validation->set_rules($rules);
+        if($this->form_validation->run()){
+            $this->db->where('SettingName','Logo');
+            $data = array('SettingValue'=>$this->input->post('MediaPath'));
+            $this->db->update('settings',$data);
+            redirect(current_url()."?success=1", $data);
+        }else{
+            $this->load->view('setting/logo', $data);
+        }
+    }
+    
+    function favicon(){
+        cekLogin();
+        $data['title'] = 'Favicon';
+        $rules = array(
+                        array(
+                            'field'=>'MediaPath',
+                            'label'=>'Favicon',
+                            'rules'=>'required'
+                        )
+        );
+        $this->form_validation->set_rules($rules);
+        if($this->form_validation->run()){
+            $this->db->where('SettingName','Favicon');
+            $data = array('SettingValue'=>$this->input->post('MediaPath'));
+            $this->db->update('settings',$data);
+            redirect(current_url()."?success=1", $data);
+        }else{
+            $this->load->view('setting/favicon', $data);
+        }
+    }
+}
